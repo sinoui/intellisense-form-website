0996d14bdca2a696850f9db7ef9fd30119f87067
duanyaqin <361711931@qq.com>
2021-03-19 17:05:17 +0800

    chore: 注释不用配置

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,40 +8,40 @@ module.exports = {
   projectName: "intellisense-form-website", // Usually your repo name.
   githubHost: "gitee.com",
   themeConfig: {
-    navbar: {
-      title: "intellisense-form",
-      logo: {
-        alt: "My Site Logo",
-        src: "img/logo.svg",
-      },
-      items: [
-        {
-          to: "docs/",
-          activeBasePath: "docs",
-          label: "文档",
-          position: "left",
-        },
-      ],
-    },
-    footer: {
-      style: "dark",
-      links: [
-        {
-          title: "Docs",
-          items: [
-            {
-              label: "Style Guide",
-              to: "docs/",
-            },
-            {
-              label: "Second Doc",
-              to: "docs/doc2/",
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-    },
+    // navbar: {
+    //   title: "intellisense-form",
+    //   logo: {
+    //     alt: "My Site Logo",
+    //     src: "img/logo.svg",
+    //   },
+    //   items: [
+    //     {
+    //       to: "docs/",
+    //       activeBasePath: "docs",
+    //       label: "文档",
+    //       position: "left",
+    //     },
+    //   ],
+    // },
+    // footer: {
+    //   style: "dark",
+    //   links: [
+    //     {
+    //       title: "Docs",
+    //       items: [
+    //         {
+    //           label: "Style Guide",
+    //           to: "docs/",
+    //         },
+    //         {
+    //           label: "Second Doc",
+    //           to: "docs/doc2/",
+    //         },
+    //       ],
+    //     },
+    //   ],
+    //   copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    // },
   },
   presets: [
     [
