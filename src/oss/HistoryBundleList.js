2cad726030a6ead68ff09f7e47bbeeba0852099e
tianyanqiu <745916729@qq.com>
2021-09-04 12:18:46 +0800

    chore: 处理历史版本安装包

---
+++ src/oss/HistoryBundleList.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import getHistoryVersionList from "./getHistoryVersionList";
+
+/**
+ * 智能表单发布包历史版本
+ */
+function HistoryBundleList({ type }) {
+  const [items, setItems] = useState([]);
+
+  useEffect(async () => {
+    const result = await getHistoryVersionList(type);
+    setItems(result ?? []);
+  }, []);
+
+  return items.length > 0 ? (
+    <ul>
+      {items.map((item) => (
+        <li key={item.name}>
+          {item.version}：&nbsp;&nbsp;<a href={item.url}>{item.name}</a>
+        </li>
+      ))}
+    </ul>
+  ) : (
+    <span>很抱歉，当前只提供最新版本安装包。</span>
+  );
+}
+
+export default HistoryBundleList;
