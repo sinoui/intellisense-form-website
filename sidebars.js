18aeb5e8291db7b9dea771374981815f3a04f387
tianyanqiu <745916729@qq.com>
2021-09-04 10:51:47 +0800

    docs: 添加下载安装包章节

--- sidebars.js
+++ sidebars.js
@@ -1,44 +1,48 @@
 module.exports = {
-  someSidebar: ["introduction", {
-    部署: [
-      "deployment",
-      "integrate",
-      "sinomatrix-docker-intellisenseform",
-    ],
-    应用程序配置: [
-      "config-custom",
-      "config-disable-modules",
-      "config-audit",
-      "config-version",
-    ],
-    插件开发: [
-      "plugin-index",
-      {
-        入门教程: [
-          "guide-setup",
-          "guide-hello",
-          "guide-button",
-          "guide-formfield",
-          "guide-watch-form-values",
-          "guide-control-button-click",
-          "guide-control-button-formitem",
-          "guide-form-style",
-          "guide-summary",
-        ],
-        高级教程: [
-          "use-app-setting",
-          "plugin-field",
-          "plugin-button",
-          "plugin-detail-page-extend",
-          "plugin-css",
-          "plugin-module-federation",
-          "plugin-test",
-        ],
-        最佳实践: ["plugin-document-structure", "plugin-naming-conventions", "plugin-dynamic-import"],
-      },
-    ],
-    样式定制: ["custom-css-pc", "custom-css-mobile", "custom-css-editor"],
-    扩展: ["extend-interface"],
-    序号组件: ["sinomatrix-sequence-table", "sinomatrix-sequence-interface"],
-  }]
+  someSidebar: [
+    "introduction",
+    "download",
+    {
+      部署: ["deployment", "integrate", "sinomatrix-docker-intellisenseform"],
+      应用程序配置: [
+        "config-custom",
+        "config-disable-modules",
+        "config-audit",
+        "config-version",
+      ],
+      插件开发: [
+        "plugin-index",
+        {
+          入门教程: [
+            "guide-setup",
+            "guide-hello",
+            "guide-button",
+            "guide-formfield",
+            "guide-watch-form-values",
+            "guide-control-button-click",
+            "guide-control-button-formitem",
+            "guide-form-style",
+            "guide-summary",
+          ],
+          高级教程: [
+            "use-app-setting",
+            "plugin-field",
+            "plugin-button",
+            "plugin-detail-page-extend",
+            "plugin-css",
+            "plugin-module-federation",
+            "plugin-test",
+          ],
+          最佳实践: [
+            "plugin-document-structure",
+            "plugin-naming-conventions",
+            "plugin-dynamic-import",
+          ],
+        },
+      ],
+      样式定制: ["custom-css-pc", "custom-css-mobile", "custom-css-editor"],
+      扩展: ["extend-interface"],
+      序号组件: ["sinomatrix-sequence-table", "sinomatrix-sequence-interface"],
+    },
+  ],
 };
