6b82989c18df35dcf1432e8f1cd55c672e2183a2
刘进行 <jackingstarliu@gmail.com>
2021-12-10 10:05:52 +0800

    fix(oss): 修复解析发布包清单错误

--- src/oss/getReleaseList.ts
+++ src/oss/getReleaseList.ts
@@ -1,5 +1,5 @@
 import type { ObjectMeta } from "ali-oss";
-import { prerelease } from "semver";
+import { prerelease, valid } from "semver";
 import {
   OSS_ACCESS_KEY_ID,
   OSS_ACCESS_KEY_SECRET,
@@ -25,16 +25,23 @@ const getClient = async () => {
 /**
  * 获取对象的版本号
  *
- * @param obj
- * @returns
+ * 从对象 name 中获取版本号，对象 name 示例：`v1.13.0/abc.zip`，则对象版本号为 `1.13.0`。
+ *
+ * 这里的对象对应 oss 中的一个文件，[对象存储](https://baike.baidu.com/item/%E5%AF%B9%E8%B1%A1%E5%AD%98%E5%82%A8/6888656)的中一个项。
+ *
+ * @param obj oss 对象
+ * @returns 返回对象的版本号
  */
 const getObjectVersion = (obj: ObjectMeta) =>
   obj.name.split("/")[0].substring(1);
 
 /**
  * 获取对象名称
- * @param obj
- * @returns
+ *
+ * 从对象 name 中获取对象名称，对象 name 示例：`v1.13.0/abc.zip`，则对象名称为 `abc.zip`。
+ *
+ * @param obj oss 对象
+ * @returns 返回对象名称
  */
 const getObjectName = (obj: ObjectMeta) => obj.name.split("/")[1];
 
@@ -50,12 +57,14 @@ const getReleaseList = async (): Promise<ReleaseItem[]> => {
 
   const client = await getClient();
   const { objects } = await client.list(null, {});
-  return objects.map((item) => ({
-    version: getObjectVersion(item),
-    name: getObjectName(item),
-    url: (item as any).url,
-    isAlpha: prerelease(getObjectVersion(item)) != null,
-  }));
+  return objects
+    .map((obj) => ({
+      version: getObjectVersion(obj),
+      name: getObjectName(obj),
+      url: (obj as any).url,
+      isAlpha: prerelease(getObjectVersion(obj)) != null,
+    }))
+    .filter((item) => valid(item.version));
 };
 
 export default memoAsyncFn(getReleaseList);
